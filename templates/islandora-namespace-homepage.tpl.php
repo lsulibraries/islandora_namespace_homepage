a772e2fca2dd4f39ef1a5bfd10274a5329f7a6b4
Jason Peak <jpeak5@lsu.edu>
2017-09-06 14:26:03 -0500

    adds collection item counts and div classes.

--- templates/islandora-namespace-homepage.tpl.php
+++ templates/islandora-namespace-homepage.tpl.php
@@ -6,7 +6,7 @@
     <?php endif; ?>
     <div class="institution-logo">
         <?php if (isset($nsHome['logo_href'])): ?>
-        <img src="<?php echo $nsHome['logo_href']; ?>" style="max-width: 100px;">
+          <img src="<?php echo $nsHome['logo_href']; ?>" style="max-width: 100px;">
         <?php endif; ?>
     </div>
     <div class="institution-about"><?php echo $nsHome['description']; ?></div>
@@ -22,7 +22,8 @@
         <?php foreach ($nsHome['collections'] as $pid => $map): ?>
           <a href='<?php echo $map['url']; ?>'>
               <li class="institution-collection-list-item">
-                  <?php echo $map['obj']->label; ?>
+                  <div class="institution-collection-list-item-count"><?php echo $map['count']; ?></div>
+                  <div class="institution-collection-list-item-label"><?php echo $map['obj']->label; ?></div>
                   <?php if ($nsHome['proxyAdmin'] && isset($map['proxy_url'])): ?>
                     <div class="institution-collection-list-item-manage-proxy">
                         <a class="institution-collection-list-item-manage-proxy-link" href="<?php echo $map['proxy_url'] ?>">Manage proxy</a>
